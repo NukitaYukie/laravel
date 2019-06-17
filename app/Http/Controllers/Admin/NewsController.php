f9a9809264135f1713bcab0eb841d821ac7164aa
YukieNukita <yukie.kondo.89+nukita@gmail.com>
2019-06-17 13:40:13 +0000

    laravel10

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,8 +7,12 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    public function add()
-   { 
-    return view('admin.news.create');
+  // 以下を追記
+  public function add()
+  {
+      return view('admin.news.create');
   }
+
 }
+
+
