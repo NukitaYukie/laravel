4a3e49ca7cbb653128a3d7edc27d9791f6820930
YukieNukita <yukie.kondo.89+nukita@gmail.com>
2020-02-03 13:32:49 +0000

    php/laravel12kadai

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,12 @@ Route::group(['prefix' => 'admin'], function() {
 
 Route::get('XXX','Admin\AAAController@bbb');
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
 
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
