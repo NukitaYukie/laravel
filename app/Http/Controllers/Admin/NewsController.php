4753285fca7e3ce9768fe544cdce5730a04bc6d4
YukieNukita <yukie.kondo.89+nukita@gmail.com>
2020-02-11 01:47:20 +0000

    php laravel課題１４再提出

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
   // 以下を追記
@@ -13,7 +15,7 @@ class NewsController extends Controller
       return view('admin.news.create');
   }
 
-}
+
 
 
 
@@ -23,10 +25,10 @@ public function create(Request $request)
    // 以下を追記
       // Varidationを行う
       $this->validate($request, News::$rules);
-​
+
       $news = new News;
       $form = $request->all();
-​
+
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
         $path = $request->file('image')->store('public/image');
@@ -34,16 +36,16 @@ public function create(Request $request)
       } else {
           $news->image_path = null;
       }
-​
+
       // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
       // フォームから送信されてきたimageを削除する
       unset($form['image']);
-​
+
       // データベースに保存する
       $news->fill($form);
       $news->save();
-​  
+  
      
 
       // admin/news/createにリダイレクトする
