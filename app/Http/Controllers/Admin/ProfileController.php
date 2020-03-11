4045f33525e962831d4e0c787ec7913dcf5b103a
YukieNukita <yukie.kondo.89+nukita@gmail.com>
2020-03-11 12:28:15 +0000

    php laravel16途中

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,31 +10,85 @@ use App\Profile;
 class ProfileController extends Controller
 {
     
-  public function add()
-  {
-      return view('admin.profile.create');
-  }
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
 
-  public function create(Request $request)
-  
+    public function create(Request $request)
+    {
+      
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
 
-  {
-      
-   $this->validate($request, Profile::$rules);
-   $news = new News;
-      
-      
-      
-      
-      return redirect('admin/profile/create');
-  }
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
 
- public function edit()
- {
- return view('admin.profile.edit');
- }
- public function update()
- {
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
+        return redirect('admin/profile/create');
+    }
+        public function index(Request $request)
+      {
+        $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのプロフィールを取得する
+          $posts = News::all();
+      }
+          return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+     
+    public function edit(Request $request)
+    {     
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);    
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    public function update(Request $request)
+    {
+        // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      
+     
+     if (isset($profile_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+        unset($profile_form['image']);
+      } elseif (isset($request->remove)) {
+        $profile->image_path = null;
+        unset($profile_form['remove']);
+      }
+      
+     
+      
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+   
  return redirect('admin/profile/edit');
  }
 }
--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.admin')
-@section('title', 'プロフィールの編集')
+@section('title', 'ニュースの編集')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール編集</h2>
-                <form action="{{ action('Admin\profileController@update') }}" method="post" enctype="multipart/form-data">
+                <h2>ニュース編集</h2>
+                <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -17,13 +17,13 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ $profile_form->title }}">
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ $profile_form->body }}</textarea>
+                            <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -31,7 +31,7 @@
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
                             <div class="form-text text-info">
-                                設定中: {{ $profile_form->image_path }}
+                                設定中: {{ $news_form->image_path }}
                             </div>
                             <div class="form-check">
                                 <label class="form-check-label">
@@ -42,7 +42,7 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-md-10">
-                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            <input type="hidden" name="id" value="{{ $news_form->id }}">
                             {{ csrf_field() }}
                             <input type="submit" class="btn btn-primary" value="更新">
                         </div>
